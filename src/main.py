ca6afe15e872bdf98b00738ea09532dd425f65b2
Samuel <ssdasilva1997@gmail.com>
2024-06-01 12:09:10 -0300

    Writing results into csv file

--- src/main.py
+++ src/main.py
@@ -5,40 +5,18 @@ import csv
 
 # Set the environment variable to point to your license file
 os.environ["GRB_LICENSE_FILE"] = "/home/samuel/Downloads/gurobi.lic"
-       
-class CommonDueDateSchedulingProblem:
-    def __init__(self, csv_filename, due_date, verbose = False):
-        self.csv_filename = csv_filename
-        self.verbose = verbose
+
+class CommonDueDateSchedulingModel:
+    def __init__(self, due_date):
         self.D_d = due_date
+        self.model = None
 
-    def _read_tasks_from_csv(self, filename):
-        with open(filename, newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar=';')
-            
-            beta = {}
-            alpha = {}
-            p = {}
+    def _initialize_model(self):
+        if self.model is None:
+            self.model = Model(name = 'Common Due Date Scheduling Problem')
+            self.model.setParam('TimeLimit', 30)
 
-            task_id = 1
-            M = 1
-            for row in spamreader:
-                p[task_id] = int(row[0])
-                alpha[task_id] =  int(row[1])
-                beta[task_id] = int(row[2].replace(';',''))
-                M = M + p[task_id]
-                
-                if self.verbose:
-                    print(f'i: {task_id}; p: {p[task_id]}; alpha = {alpha[task_id]}; beta: {beta[task_id]}')
-
-                task_id = task_id + 1
-        
-        return p, alpha, beta, M
-        
     def _add_input_variables(self, num_tasks):
-        self.model = Model(name = 'Common Due Date Scheduling Problem')
-        self.model.setParam('TimeLimit', 10)
-
         e = {}
         t = {}
         d = {}
@@ -79,6 +57,43 @@ class CommonDueDateSchedulingProblem:
             quicksum(((alpha[i+1]*e[i+1]) + (beta[i+1]*t[i+1])) for i in range(0, num_tasks)),
             sense=GRB.MINIMIZE
         )
+    
+    def get_model(self, num_tasks, p, alpha, beta, M):
+        self._initialize_model()
+        e, t, d, tau, J = self._add_input_variables(num_tasks)
+        self._add_constraints(J, tau, d, M,  e, t, p, num_tasks)
+        self._define_objective_function(alpha, e, beta, t, num_tasks)
+
+        return self.model, e, t, d, tau, J
+
+class CommonDueDateSchedulingProblem:
+    def __init__(self, csv_filename, due_date, verbose = False):
+        self.csv_filename = csv_filename
+        self.verbose = verbose
+        self.D_d = due_date
+
+    def _read_tasks_from_csv(self, filename):
+        with open(filename, newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar=';')
+            
+            beta = {}
+            alpha = {}
+            p = {}
+
+            task_id = 1
+            M = 1
+            for row in spamreader:
+                p[task_id] = int(row[0])
+                alpha[task_id] =  int(row[1])
+                beta[task_id] = int(row[2].replace(';',''))
+                M = M + p[task_id]
+                
+                if self.verbose:
+                    print(f'i: {task_id}; p: {p[task_id]}; alpha = {alpha[task_id]}; beta: {beta[task_id]}')
+
+                task_id = task_id + 1
+        
+        return p, alpha, beta, M
 
     def _order_task_by_delay_penalty(self, beta):
         # Create a list of tuples (index, penalty_for_delay)
@@ -92,8 +107,9 @@ class CommonDueDateSchedulingProblem:
         
         return sorted_tasks
     
-    def _define_initial_condition(self, beta, J):
-        taks_sorted_by_delay_penalty = self._order_task_by_delay_penalty(beta)
+    def _define_initial_condition(self, beta, alpha, J):
+        beta_over_alpha = {key: beta[key] / alpha[key] for key in alpha}
+        taks_sorted_by_delay_penalty = self._order_task_by_delay_penalty(beta_over_alpha)
 
         if self.verbose:
             print('Taks sorted by delay penalty:', taks_sorted_by_delay_penalty)
@@ -241,28 +257,32 @@ class CommonDueDateSchedulingProblem:
         return delayed_tasks, early_tasks
 
     def _neighborhood_change(self, current_best_solution, candidate, k):
-        current_best_solution_input, current_best_solution_model = current_best_solution
-        candidate_input, candidate_model = candidate
+        current_best_solution_J, current_best_solution_e,current_best_solution_t,current_best_solution_model = current_best_solution
+        candidate_J, candidate_e,candidate_t, candidate_model = candidate
         current_best_value = current_best_solution_model.ObjVal
         candidate_value = candidate_model.ObjVal
 
         if candidate_value < current_best_value:
             best_solution_model = candidate_model
-            best_solution_input = candidate_input
+            best_solution_J = candidate_J
+            best_solution_e = candidate_e
+            best_solution_t = candidate_t
             k = 1
         else:
             best_solution_model = current_best_solution_model
-            best_solution_input = current_best_solution_input
+            best_solution_J = current_best_solution_J
+            best_solution_e = current_best_solution_e
+            best_solution_t = current_best_solution_t
             k = k + 1
 
-        return best_solution_input, best_solution_model, k
+        return best_solution_J, best_solution_e, best_solution_t, best_solution_model, k
 
     def _shake(self, x, k):
         if k == 1:
             return self._shake_on_neighborhood_1(x)
         elif k == 2:
             return self._shake_on_neighborhood_2(x)
-        return self._shake_on_neighborhood_3(x, k)
+        return self._shake_on_neighborhood_3(x)
 
     def _fix_J_Matrix_from_J_vector(self, J_vector, J_matrix, num_tasks):
         for task_id in range(1, num_tasks+1):
@@ -283,10 +303,15 @@ class CommonDueDateSchedulingProblem:
         
         return converted_array
 
-    def _fix_J_matrix(self, J, num_tasks):
+    def _fix_current_J_matrix(self, J, num_tasks):
         for i in range(1, num_tasks+1):
             for j in range(1, num_tasks+1):
                 self._fix_variable(J[i][j], J[i][j].X)
+
+    def _fix_J_matrix(self, J_to_fix, J_to_copy, num_tasks):
+        for i in range(1, num_tasks+1):
+            for j in range(1, num_tasks+1):
+                self._fix_variable(J_to_fix[i][j], J_to_copy[i][j].X)
 
     # Randomly select a delayed task and put in the first order
     def _shake_on_neighborhood_1(self, x):
@@ -328,7 +353,7 @@ class CommonDueDateSchedulingProblem:
         self.model.optimize()
 
     # Randomly select a early task and put in the last order
-    def _shake_on_neighborhood_3(self, x, k):
+    def _shake_on_neighborhood_3(self, x):
         e, t, J = x
         num_tasks = len(e)
 
@@ -344,13 +369,13 @@ class CommonDueDateSchedulingProblem:
         # Here we call model optimize only to compute objective function from new shaked solution
         self.model.optimize()
 
-    def _BVNS(self, x, k_max, max_iterations):
+    def _BVNS(self, input, k_max, max_iterations):
         num_it = 0
-        e, t, J = x
-        num_tasks = len(e)
-        self._fix_J_matrix(J, num_tasks)
-        best_solution_input = J.copy()
-        best_solution_model = self.model.copy()
+        e, t, J, num_tasks, p, alpha, beta, M = input
+        x = (e, t, J)
+        
+        best_solution_model, best_solution_e,best_solution_t,_,_, best_solution_J = CommonDueDateSchedulingModel(self.D_d).get_model(num_tasks, p, alpha, beta, M)
+        self._fix_J_matrix(best_solution_J, J, num_tasks)
         # Just call optimize here to compute value of objective function
         best_solution_model.optimize()
 
@@ -359,46 +384,42 @@ class CommonDueDateSchedulingProblem:
             while k <= k_max:
                 self._shake(x, k)
                 self._fix_and_optimize(J, num_tasks)  
-                J, self.model, k = self._neighborhood_change((best_solution_input, best_solution_model), (J, self.model), k)
-                self._fix_J_matrix(J, num_tasks)
-                best_solution_input = J
-                best_solution_model = self.model.copy()
+                J, e, t, self.model, k = self._neighborhood_change((best_solution_J, best_solution_e,best_solution_t, best_solution_model), (J, e, t, self.model), k)
+                self._fix_current_J_matrix(J, num_tasks)
+                self.model.optimize()
+                x = (e, t, J)
+                best_solution_model, best_solution_e,best_solution_t,best_solution_d,best_solution_tau, best_solution_J = CommonDueDateSchedulingModel(self.D_d).get_model(num_tasks, p, alpha, beta, M)
+                self._fix_J_matrix(best_solution_J, J, num_tasks)
                 best_solution_model.optimize() # Just call optimize here to compute value of objective function with fixed J matrix
                 num_it = num_it + 1
-                if (num_it > max_iterations):
+                if (num_it >= max_iterations):
                     break
 
+        return best_solution_model, best_solution_e, best_solution_t, best_solution_d, best_solution_tau, best_solution_J
+
+    def write_best_solution_found(self, J_matrix, num_tasks):
+        with open("AnaSamuel.csv", "w") as file:
+            J_vector = self._get_array_from_J_matrix(J_matrix, num_tasks)
+            for task in J_vector:
+                file.write(f"{task}")
+                if task != J_vector[-1]:
+                    file.write(f", ")
 
     def compute_solution(self):
         p, alpha, beta, M = self._read_tasks_from_csv(self.csv_filename)
         num_tasks = len(p)
-        e, t, d, tau, J = self._add_input_variables(num_tasks)
-        self._add_constraints(J, tau, d, M,  e, t, p, num_tasks)
-        self._define_objective_function(alpha, e, beta, t, num_tasks)
-        self._define_initial_condition(beta, J)
+        self.model, e, t, d, tau, J = CommonDueDateSchedulingModel(self.D_d).get_model(num_tasks, p, alpha, beta, M)
+        self._define_initial_condition(beta, alpha, J)
         self.model.optimize()
-        self._BVNS((e, t, J), 3, 10)
-        #print("Relax and fix")
-        #self._relax_and_fix(J, num_tasks)
-        #self._fix_and_optimize(J, num_tasks)
-        
-        for v in self.model.getVars():
-            print(f"{v.VarName} {v.X:g}")
+        _, _, _, _, _, best_solution_J = self._BVNS((e, t, J, num_tasks, p, alpha, beta, M), 3, 20)
 
-        print(f"Obj: {self.model.ObjVal:g}")
+        if self.verbose:
+            print(f"Obj: {self.model.ObjVal:g}")
 
-        # test shake method
+            for v in self.model.getVars():
+                print(f"{v.VarName} {v.X:g}")
 
-        #self._shake((e, t, J), 1)
-        #self._shake((e, t, J), 2)
-        #self._shake((e, t, J), 3)
-        #self.model.computeIIS()
-        #self.model.write(f"model.ilp")
-
-        for v in self.model.getVars():
-            print(f"{v.VarName} {v.X:g}")
-
-        print(f"Obj: {self.model.ObjVal:g}")
+        self.write_best_solution_found(best_solution_J, num_tasks)
 
         self.model.dispose()
 
